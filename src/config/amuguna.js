fcf922da0d1cac2bcdb10fdb9faef72ed2e11be0
jhyjj <jhyj3768@gmail.com>
2024-01-27 16:46:07 +0900

    update : server update (fly.io)

--- src/config/amuguna.js
+++ src/config/amuguna.js
@@ -1,3 +1,6 @@
 // export const API_URL = "https://hhh-server.herokuapp.com" //헤로쿠 서버
-export const API_URL = "http://localhost:3001" //로컬 서버
+//export const API_URL = "http://localhost:3001" //로컬 서버
+export const API_URL = "https://hhh-server.fly.dev" //fly.io
+
+
  
